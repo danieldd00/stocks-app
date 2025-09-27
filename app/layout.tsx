91c9719b4ee8237dd7226fc42422f9624ef98c4a
Daniel Dizdarevic <daniel@threedark.com>
2025-09-26 19:50:26 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark1">
+    <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
